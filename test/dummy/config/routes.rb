cb94c8d68bd3d34f938739ef10f2582e50ffecca
Nicolai Bach Woller <woller@traels.it>
2023-06-30 09:26:27 +0200

    Chore. Scaffold a User model and controller.

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
   mount Kommandant::Engine => "/kommandant"
+
+  root to: "users#index"
 end
--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_19_084925) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_30_071829) do
   create_table "users", force: :cascade do |t|
     t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
