bfc83ae576e60edf7e1917cea9e1a0ab9660c793
Nicolai Bach Woller <woller@traels.it>
2023-09-12 10:45:47 +0200

    Feature. Add Kommandant::RecentCommands controller concern.

---
+++ app/controllers/concerns/kommandant/recent_commands.rb
@@ -0,0 +1,16 @@
+module Kommandant::RecentCommands
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_recent_commands
+  end
+
+  def set_recent_commands
+    # TODO: Make current_user configurable
+    return unless current_user.present?
+
+    @recent_commands ||= current_user.recent_commands.elements.map do |command_id|
+      Kommandant::Command.find(command_id)
+    end
+  end
+end
