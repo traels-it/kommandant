501d7fe478aff42afaa70e3e5b40bf8d9423f1a2
Nicolai Bach Woller <woller@traels.it>
2023-09-13 13:04:13 +0200

    Refactor.

--- app/controllers/kommandant/commands/searches_controller.rb
+++ app/controllers/kommandant/commands/searches_controller.rb
@@ -4,12 +4,7 @@ class Kommandant::Commands::SearchesController < ApplicationController
   before_action :set_command
 
   def show
-    # Pagy would be most efficiently used on the output of the search method - but we need to check, that the user is
-    # actually allowed to view the resource
-    @resources = @command.resource_class.constantize.search(params[:query])
-      .select { |resource| Kommandant.config.search_result_filter_lambda.call(current_ability, resource) }
-
-    @results = @resources.map do |resource|
+    @results = filtered_resources.map do |resource|
       Kommandant::Commands::SearchResult.new(command: @command, resource: resource)
     end
 
@@ -20,5 +15,13 @@ class Kommandant::Commands::SearchesController < ApplicationController
 
   def set_command
     @command = Kommandant::Command.find(params[:command_id])
+  end
+
+  def filtered_resources
+    # Pagy would be most efficiently used on the output of the search method - but we need to check, that the user is
+    # actually allowed to view the resource
+    @resources ||= @command.resource_class.constantize.search(params[:query]).filter do |resource|
+      Kommandant.config.search_result_filter_lambda.call(current_ability, resource)
+    end
   end
 end
